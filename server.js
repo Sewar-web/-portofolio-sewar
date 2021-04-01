87521928a0a7e3303ea8f9eaff5ce98c10c41ae9
Sewar-web <s.maqableh@yahoo.com>
2021-04-01 19:57:53 +0300

    sewarr

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ server.use(express.static('./public'));
 
 
 // localhost:7777/test ->http request (/test->route)
-server.get('/s', (req ,res) =>{
+server.get('7', (req ,res) =>{
 res.send('sewar home')
 })
 
